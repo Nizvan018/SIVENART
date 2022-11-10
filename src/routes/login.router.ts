19827fed3e085457a42de538d96a149b6832d1c7
Nizvan MR <njr01397@gmail.com>
2022-11-09 20:08:33 -0600

    Add a new login view

--- src/routes/login.router.ts
+++ src/routes/login.router.ts
@@ -1,9 +1,9 @@
 import {Router} from 'express';
 
+import {login} from '../controllers/login.controller';
+
 const router:Router = Router();
 
-router.get('/', (req, res) => {
-    res.send('A');
-});
+router.get('/', login);
 
 export default router;
