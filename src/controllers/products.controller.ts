9062ba84ecf2467edf458eed1132a71696c745ed
Nizvan MR <njr01397@gmail.com>
2022-11-27 15:44:34 -0600

    Add(views, routes, css): Add the view for see_products and its routes

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -2,6 +2,11 @@ import { Response, Request } from "express";
 
 
 /** Funciones para el renderizado de vistas: */
+
+export function ver_productos(req:Request, res:Response){
+    res.render('products/see_products');
+}
+
 export function registrar_producto(req:Request, res:Response){
     res.render('products/register_products');
 }
