a0edc0bccfa78e3f6a63e4cdc13af9698d24a9b9
Nizvan MR <njr01397@gmail.com>
2022-12-04 20:32:13 -0600

    Feat(views, js, controllers, routes): Add feature of the user page

--- src/public/assets/js/navbar.js
+++ src/public/assets/js/navbar.js
@@ -1,4 +1,6 @@
 let nav_aux = document.getElementById('nav_aux');
+let option1 = document.getElementById('option1');
+let option2 = document.getElementById('option2');
 let option3 = document.getElementById('option3');
 
 /** Función para mostrar y ocultar la navbar auxliar */
@@ -6,3 +8,13 @@ option3.addEventListener('click', () => {
     nav_aux.classList.toggle('d-flex');
     nav_aux.classList.toggle('d-none');
 });
+
+/** Función para ocultar el carrito y el usuario en la vista de signin */
+function register_client(){
+    if(window.location.pathname.includes('signin')){
+        option1.hidden = true;
+        option2.hidden = true;
+    }
+}
+
+register_client();
