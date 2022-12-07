da93efd0d34cddafb834d451283e5b177cba924d
Nizvan MR <njr01397@gmail.com>
2022-12-07 17:40:49 -0600

    Add(views, js): Add validation for payment-cart view

--- src/public/assets/js/products/payment-cart.js
+++ src/public/assets/js/products/payment-cart.js
@@ -4,33 +4,18 @@ const $btn_register = document.getElementById('btn_register');
 const $form = document.getElementsByTagName('form');
 const $numero_tarjeta = document.getElementById('numeroTarjeta');
 const $cvv = document.getElementById('cvv');
-const $nombre_tarjeta = document.getElementById('nombreTarjeta');
+const $nombre_tarjeta = document.getElementById('nombre_tarjeta');
 /** Regex: */
 const numero_tarjeta_regex = /^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}/;
 const cvv_regex = /^[0-9]{3}/;
 
-/** Función para validar los datos de la tarjeta: */
 $btn_register.addEventListener('click', () => {
     if(numero_tarjeta_regex.test($numero_tarjeta.value) &&
     cvv_regex.test($cvv.value) &&
     $nombre_tarjeta.value.trim().length > 0){
         // $form[0].submit();
-        console.log('si');
+        console.log('no');
     } else{
         window.alert('Introduzca correctamente los datos solicitados');
     }
 });
-
-/** Función para calcular los años del input: */
-function calcular_anio(){
-    var select = document.getElementById('anoVencimiento');
-        var anioActual = new Date().getFullYear();
-        for (var i = anioActual; i <= anioActual + 15; i++) {
-            var option = document.createElement('option');
-            option.value = i;
-            option.innerHTML = i;
-            select.appendChild(option);
-        }
-}
-
-calcular_anio();
