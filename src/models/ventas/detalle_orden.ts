2c0d392448694cad21ec9fcfa006977d46a8d741
Naim <forestvtc@gmail.com>
2022-12-07 19:05:06 -0600

    Fix(models) Addition cantidad in orden_detalle table

--- src/models/ventas/detalle_orden.ts
+++ src/models/ventas/detalle_orden.ts
@@ -2,6 +2,10 @@ import { DataTypes, or } from "sequelize";
 import { sequelize } from "../../database/database.config";
 
 export const orden_detalle = sequelize.define('orden_detalle', {
+    cantidad: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }
 }, {
     timestamps: true,
 })
